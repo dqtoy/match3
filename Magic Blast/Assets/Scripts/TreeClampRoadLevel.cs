bc095b4ec91d9283f01639a6f571da3e69c1d08f
Maxim <marad747@yandex.ua>
2017-09-25 11:14:10 +0300

    add graphics

--- Magic Blast/Assets/Scripts/TreeClampRoadLevel.cs
+++ Magic Blast/Assets/Scripts/TreeClampRoadLevel.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TreeClampRoadLevel : MonoBehaviour {
 
@@ -8,6 +9,8 @@ public class TreeClampRoadLevel : MonoBehaviour {
 	public GameObject active;
 	public GameObject inactive;
 	public GameObject check;
+    public GameObject light;
+    public Text LevelsLeft;
 
 	public int level = 1;
 
@@ -28,14 +31,25 @@ public class TreeClampRoadLevel : MonoBehaviour {
 		int currentLevel = ChallengeController.instanse.currentSelectedClambLevel;
 		if (currentLevel == level) {
 			active.SetActive (true);
-		} else if (level > currentLevel) {
+            light.SetActive(true);
+        } else if (level > currentLevel) {
 			inactive.SetActive (true);
-		} else {
+            light.SetActive(false);
+        } else {
 			active.SetActive (true);
 			check.SetActive (true);
-		}
+            light.SetActive(false);
+        }
+        if(currentLevel == 1)
+            LevelsLeft.text = 5.ToString() + " Levels Left";
+        else
+            if(currentLevel == 5)
+            LevelsLeft.text = ( 6 - currentLevel ).ToString() + " Level Left";
+                else
+            LevelsLeft.text = ( 6 - currentLevel ).ToString() + " Levels Left";
 
-	}
+
+    }
 	// Update is called once per frame
 	void Update () {
 		
